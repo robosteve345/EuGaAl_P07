dbc7c7b1700e4081f4f91c7acea12d69562da93d
robosteve <86156294+robosteve345@users.noreply.github.com>
2023-07-21 15:29:04 +0200

    Added new 159 maps

--- DESY P07 April 2023/Analysis/Crysalis_analysis.py
+++ DESY P07 April 2023/Analysis/Crysalis_analysis.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -12,8 +11,9 @@ matplotlib.rcParams['font.family'] = "sans-serif"
 matplotlib.rcParams['pcolor.shading']
 matplotlib.rc('text', usetex=True)
 
+"""Print unwarped CrysAlisPro intensity maps"""
 
-def imgplot(imgpath, resolution, lat_a, lat_c, ylim, xlim=0):
+def imgplot(imgpath, resolution, lat_a, lat_c, ylim, title, xlim=0):
     """
     :input:
     lat_a, lat_c: tetragonal unit cell parameters
@@ -37,19 +37,23 @@ def imgplot(imgpath, resolution, lat_a, lat_c, ylim, xlim=0):
     fig, ax = plt.subplots(figsize=(9, 7))
     cmap = cm.get_cmap("viridis")
     im = ax.pcolormesh(qx, qy, data, cmap=cmap,
-                       norm=LogNorm(vmin=10, vmax=np.max(data) * 0.5)
+                       norm=LogNorm(vmin=10, vmax=np.max(data))
                        )
     ax.set_ylim([-ylim, ylim])
+    # ax.set_title(''.format(title))
     ax.set_xlim([xlim, qpixelx * (NX / 2.)])
     ax.set_xlabel("H (rlu)")
     ax.set_ylabel("L (rlu)")
+    ax.grid(True, which='both', axis='both', linestyle='-', color='black')
     fig.colorbar(im, ax=ax)
     plt.show()
 
 
 def main():
-    list = ["0p5_159_H0L_0.5A.img"]  #For batch-processing, define list with img-names
-    imgplot(list[0], 0.5, 4.3486, 10.9766, xlim=-1, ylim=14)  #Process by iterating through given list with imgplot-function
+    print(__name__)
+    list = ["0p5_159_H0L_0.5A.img", "0p5_159_H1L_0.5A.img", "0p5_159_HK0_0.5A.img", "0p5_159_Hm1L_0.5A.img"]  #For batch-processing, define list with img-names
+    imgplot(list[3], 0.5, 4.3262, 10.929, xlim=-1, ylim=14, title='159_Hm1L')  #Process by iterating through given list with imgplot-function
+
 
 if __name__ == "__main__":
     main()
